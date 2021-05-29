6efe7c21e1db9b5fc0243cbe2a33e3438cd49f38
Ambaram <34491921+Ambaram@users.noreply.github.com>
2021-05-30 00:02:57 +0530

    Update QuickRead.js

--- QuickRead.js
+++ QuickRead.js
@@ -10,19 +10,19 @@ function execute() {
             switch (this.id) {
                 case "ezbtn":
                     setInterval(easytime, 1000);
-                    files.open("GET", "https://github.com/Ambaram/QuickRead/blob/master/hard2.txt", true);
+                    files.open("GET", https://github.com/Ambaram/QuickRead/blob/master/hard2.txt, true);
                     files.send(null);
                     break;
                 case "mdmbtn":
                     document.getElementById("medcon").style.display = "block";
                     setInterval(mediumtime, 1000);
-                    files.open("GET", "https://github.com/Ambaram/QuickRead/blob/master/med1.txt", true);
+                    files.open("GET", https://github.com/Ambaram/QuickRead/blob/master/med1.txt, true);
                     files.send(null);
                     break;
                 case "hrdbtn":
                     document.getElementById("hardcon").style.display = "block";
                     setInterval(hardtime, 1000);
-                    files.open("GET", "https://github.com/Ambaram/QuickRead/blob/master/hard1.txt", true);
+                    files.open("GET", https://github.com/Ambaram/QuickRead/blob/master/hard1.txt, true);
                     files.send(null);
                     break;
             }
