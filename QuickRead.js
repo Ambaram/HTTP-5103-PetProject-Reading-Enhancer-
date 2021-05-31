162e74b35f1f66672d2ad9275f3e9097d1721da8
Ambaram <34491921+Ambaram@users.noreply.github.com>
2021-05-31 15:20:04 +0530

    Update QuickRead.js

--- QuickRead.js
+++ QuickRead.js
@@ -2,9 +2,6 @@ window.onload = execute;
 function execute() {
     var article = document.getElementById("Text")
     var elem = document.getElementsByName("level");
-    url1="https://raw.githubusercontent.com/Ambaram/QuickRead/master/hard2.txt";
-    url2="https://raw.githubusercontent.com/Ambaram/QuickRead/master/hard1.txt";
-    url3="https://raw.githubusercontent.com/Ambaram/QuickRead/master/med1.txt";
 
     for (i = 0; i < elem.length; i++) {
         elem[i].onclick = display;
@@ -14,19 +11,19 @@ function execute() {
             switch (this.id) {
                 case "ezbtn":
                     setInterval(easytime, 1000);
-                    files.open("GET", "url1", true);
+                    files.open("GET", "hard1,txt", true);
                     files.send(null);
                     break;
                 case "mdmbtn":
                     document.getElementById("medcon").style.display = "block";
                     setInterval(mediumtime, 1000);
-                    files.open("GET", "url2", true);
+                    files.open("GET", "hard2.txt", true);
                     files.send(null);
                     break;
                 case "hrdbtn":
                     document.getElementById("hardcon").style.display = "block";
                     setInterval(hardtime, 1000);
-                    files.open("GET", "url3", true);
+                    files.open("GET", "med1.txt", true);
                     files.send(null);
                     break;
             }
